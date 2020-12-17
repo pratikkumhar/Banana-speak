dd8f5c68d423c9fc39f176be7c3480637c3b4824
Pratik Kumhar <pratikkumhar79@gmail.com>
2020-12-17 15:34:53 +0530

    update- input,output

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input")
+var output = document.querySelector("#output")
+
+
+console.log(txtInput)
+
+function clickHandler(){
+    output.innerText = "abcd " + txtInput.value;
+};
+
+
+btnTranslate.addEventListener("click", clickHandler)
+
+// txtInput.addEventListener("input",)
